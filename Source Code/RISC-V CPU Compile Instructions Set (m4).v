49782d294c0c4cb6ac5bf64daab173cdc8ff93b5
Nguyễn Minh Đức <100473300+Duckie2369@users.noreply.github.com>
2024-11-18 12:47:21 +0700

    Complete the rest of the Decoder
    Add some basic instruction (add, addi, beq, etc.)

--- Source Code/RISC-V CPU Compile Instructions Set (m4).v
+++ Source Code/RISC-V CPU Compile Instructions Set (m4).v
@@ -68,13 +68,10 @@
    $opcode[6:0] = $instr[6:0];
    
    //Validation of these fields
-   $rd_valid = ~($is_s_instr || $is_b_instr || $instr[11:7] == 5'b0);
+   $rd_valid  = ~($is_s_instr || $is_b_instr || $instr[11:7] == 5'b0);
    $imm_valid = ~$is_r_instr;
    $rs1_valid = ~($is_u_instr || $is_j_instr);
    $rs2_valid = ($is_r_instr || $is_s_instr || $is_b_instr);
-   
-   //Turn off warning for unused variables in LOG
-   `BOGUS_USE($rd $rd_valid $rs1 $rs1_valid $rs2 $rs2_valid $funct3 $imm_valid $opcode) 
    
    // Extracting the immediate field from the instruction
    $imm[31:0] = $is_i_instr ? {{21{$instr[31]}}, $instr[30:20]} :
@@ -83,6 +80,23 @@
                 $is_u_instr ? {$instr[31], $instr[30:20], $instr[19:12], 12'b0} :
                 $is_j_instr ? {{11{$instr[31]}}, $instr[19:12], {2{$instr[20]}}, $instr[30:21], 1'b0} :
                 32'b0 ;
+   
+   // A single bit vector signal to determine the specific instruction
+   $dec_bits[10:0] = {$instr[30],$funct3,$opcode};
+   
+   // Some basic instruction 
+   $is_beq  = $dec_bits ==? 11'bx_000_1100011;
+   $is_bne  = $dec_bits ==? 11'bx_001_1100011;
+   $is_blt  = $dec_bits ==? 11'bx_100_1100011;
+   $is_bge  = $dec_bits ==? 11'bx_101_1100011;
+   $is_bltu = $dec_bits ==? 11'bx_110_1100011;
+   $is_bgeu = $dec_bits ==? 11'bx_111_1100011;
+   $is_addi = $dec_bits ==? 11'bx_000_0010011;
+   $is_add  = $dec_bits ==  11'b0_000_0110011;
+   
+   
+   //Turn off warning for unused variables in LOG
+   `BOGUS_USE($rd $rd_valid $rs1 $rs1_valid $rs2 $rs2_valid $imm_valid) 
    
    // Assert these to end simulation (before Makerchip cycle limit).
    *passed = 1'b0;
