824022a927631cbf38a5e0537dc1b83d1694daa3
Nguyễn Minh Đức <100473300+Duckie2369@users.noreply.github.com>
2024-12-12 09:20:11 +0700

    Add range for $is_add components

--- Source Code/RISC-V CPU Compile Instructions Set (m4).v
+++ Source Code/RISC-V CPU Compile Instructions Set (m4).v
@@ -103,7 +103,7 @@
    
    // ALU
    $result[31:0] = $is_addi ? $src1_value + $imm :
-                   $is_add  ? $src1_value + $src2_value:
+                   $is_add  ? $src1_value[31:0] + $src2_value[31:0]:
                    32'b0;
    
    // Adding a MUX to select either result or $ld_data depending on instruction
