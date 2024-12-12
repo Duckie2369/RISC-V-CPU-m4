2834045aeb72b8ef23048874d946b80a1098c7f3
Nguyễn Minh Đức <100473300+Duckie2369@users.noreply.github.com>
2024-12-12 11:15:45 +0700

    Conditional Branch + Update PC

--- Source Code/RISC-V CPU Compile Instructions Set (m4).v
+++ Source Code/RISC-V CPU Compile Instructions Set (m4).v
@@ -6,34 +6,9 @@
 
 
 
-   //---------------------------------------------------------------------------------
-   // /====================\
-   // | Sum 1 to 9 Program |
-   // \====================/
-   //
-   // Program to test RV32I
-   // Add 1,2,3,...,9 (in that order).
-   //
-   // Regs:
-   //  x12 (a2): 10
-   //  x13 (a3): 1..10
-   //  x14 (a4): Sum
-   // 
-   m4_asm(ADDI, x14, x0, 0)             // Initialize sum register a4 with 0
-   m4_asm(ADDI, x12, x0, 1010)          // Store count of 10 in register a2.
-   m4_asm(ADDI, x13, x0, 1)             // Initialize loop count register a3 with 0
-   // Loop:
-   m4_asm(ADD, x14, x13, x14)           // Incremental summation
-   m4_asm(ADDI, x13, x13, 1)            // Increment loop count by 1
-   m4_asm(BLT, x13, x12, 1111111111000) // If a3 is less than a2, branch to label named <loop>
-   // Test result value in x14, and set x31 to reflect pass/fail.
-   m4_asm(ADDI, x30, x14, 111111010100) // Subtract expected value of 44 to set x30 to 1 if and only iff the result is 45 (1 + 2 + ... + 9).
-   m4_asm(BGE, x0, x0, 0) // Done. Jump to itself (infinite loop). (Up to 20-bit signed immediate plus implicit 0 bit (unlike JALR) provides byte address; last immediate bit should also be 0)
-   m4_asm_end()
-   m4_define(['M4_MAX_CYC'], 50)
-   //---------------------------------------------------------------------------------
+   m4_test_prog()
 
-
+   
 
 \SV
    m4_makerchip_module   // (Expanded in Nav-TLV pane.)
@@ -45,8 +20,11 @@
    
    // YOUR CODE HERE
    // Program Counter
-   $next_pc[31:0] = $reset ? 32'b0 : $pc[31:0] + 1;
-   $pc[31:0] = >>1$next_pc[31:0];
+   $pc[31:0] = >>1$next_pc;
+   $next_pc[31:0] = $reset ? 32'b0 :
+                    $taken_br ? $br_tgt_pc :
+                    $pc[31:0] + 1;
+   
    
    //Instruction Memory
    `READONLY_MEM($pc, $$instr[31:0])
@@ -99,18 +77,30 @@
    
    
    //Turn off warning for unused variables in LOG
-   `BOGUS_USE($rd $rd_valid $rs1 $rs1_valid $rs2 $rs2_valid $imm_valid) 
+   `BOGUS_USE($rd $rd_valid $rs1 $rs1_valid $rs2 $rs2_valid $funct3 $imm $imm_valid);
    
    // ALU
    $result[31:0] = $is_addi ? $src1_value + $imm :
                    $is_add  ? $src1_value[31:0] + $src2_value[31:0]:
                    32'b0;
    
-   // Adding a MUX to select either result or $ld_data depending on instruction
+   // Select result or $ld_data depending on instruction using MUX
    $result_write_rf[31:0] = $is_load ? $ld_data[31:0] : $result;
    
+   // Conditional Branch Instruction MUX
+   $taken_br = $is_beq ? ($src1_value == $src2_value ? 1'b1 : 1'b0) :
+               $is_bne ? ($src1_value != $src2_value ? 1'b1 : 1'b0) :
+               $is_blt ? (($src1_value < $src2_value) ^ ($src1_value[31] != $src2_value[31]) ? 1'b1 : 1'b0) :
+               $is_bge ? (($src1_value >= $src2_value) ^ ($src1_value[31] != $src2_value[31]) ? 1'b1 : 1'b0) :
+               $is_bltu ? ($src1_value < $src2_value ? 1'b1 : 1'b0) :
+               $is_bgeu ? ($src1_value >= $src2_value ? 1'b1 : 1'b0) :
+               1'b0 ;
+   
+   // Coding the next instruction location for branching
+   $br_tgt_pc[31:0] = $pc[31:0] + $imm;
+   
    // Assert these to end simulation (before Makerchip cycle limit).
-   *passed = 1'b0;
+   m4+tb()
    *failed = *cyc_cnt > M4_MAX_CYC;
    
    m4+rf(32, 32, $reset, $rd_valid, $rd[4:0], $result_write_rf[31:0], $rs1_valid, $rs1, $src1_value, $rs2_valid, $rs2, $src2_value)
