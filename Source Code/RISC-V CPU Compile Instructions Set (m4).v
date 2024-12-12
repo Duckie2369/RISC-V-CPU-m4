c68c655bd0c4172e913ac364ae97d47408586a72
Nguyễn Minh Đức <100473300+Duckie2369@users.noreply.github.com>
2024-12-12 11:47:50 +0700

    Finish all instructions expressions + Add Ref card

--- Source Code/RISC-V CPU Compile Instructions Set (m4).v
+++ Source Code/RISC-V CPU Compile Instructions Set (m4).v
@@ -102,8 +102,44 @@
    `BOGUS_USE($rd $rd_valid $rs1 $rs1_valid $rs2 $rs2_valid $funct3 $imm $imm_valid);
    
    // ALU
+   
+   // SLTU and SLTI (set if less than,unsigned) results:
+   $sltu_rslt[31:0] = {31'b0, $src1_value < $src2_value};
+   $sltiu_rslt[31:0] = {31'b0, $src1_value < $imm};
+   
+   // SRA and SRAI (shift right,arithmetic) results:
+   // sign-extended src1
+   $sext_src1[63:0] = {{32{$src1_value[31]}}, $src1_value};
+   // 64-bit sign-extended results. to be truncated
+   $sra_rslt[63:0] = $sext_src1 >> $src2_value[4:0];
+   $srai_rslt[63:0] = $sext_src1 >> $imm[4:0];
+   
    $result[31:0] = $is_addi ? $src1_value + $imm :
                    $is_add  ? $src1_value[31:0] + $src2_value[31:0]:
+                   $is_andi ? $src1_value & $imm:
+                   $is_ori ? $src1_value | $imm:
+                   $is_xori ? $src1_value ^ $imm:
+                   $is_addi ? $src1_value + $imm:
+                   $is_slli ? $src1_value << $imm[5:0]:
+                   $is_srli ? $src1_value >> $imm[5:0]:
+                   $is_and ? $src1_value & $src2_value:
+                   $is_or ? $src1_value | $src2_value:
+                   $is_xor ? $src1_value ^ $src2_value:
+                   $is_add ? $src1_value + $src2_value:
+                   $is_sub ? $src1_value - $src2_value:
+                   $is_sll ? $src1_value << $src2_value:
+                   $is_srl ? $src1_value >> $src2_value:
+                   $is_sltu ? $sltu_rslt:
+                   $is_sltiu ? $sltiu_rslt:
+                   $is_lui ? {$imm[31:12], 12'b0}:
+                   $is_auipc ? $pc + {$imm[31:12], 12'b0}:
+                   $is_jal ? $pc + 32'd4:
+                   $is_jalr ? $pc + 32'd4:
+                   $is_slt ? (($src1_value[31] == $src2_value[31]) ? $sltu_rslt : {31'b0, $src1_value[31]}):
+                   $is_slti ? (($src1_value[31] == $imm[31]) ? $sltu_rslt : {31'b0, $src1_value[31]}):
+                   $is_sra ? $sra_rslt[31:0]:
+                   $is_srai ? $srai_rslt[31:0]:
+                   ($is_load || $is_s_instr) ? $src1_value + $imm:
                    32'b0;
    
    // Select result or $ld_data depending on instruction using MUX
